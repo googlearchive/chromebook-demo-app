34963f0aea0ba1fdac5c914398d971bed04d973a
Daichi Hirono <hirono@chromium.org>
2013-07-11 18:24:34 -0700

    Revert to b59d3771f.

--- main/js/main.js
+++ main/js/main.js
@@ -100,7 +100,7 @@ var HangoutsPage = function(root) {
   AppPage.call(this, root, 'hangouts-app');
   this.effectIndex_ = 0;
   this.frame_ = 0;
-  // this.renderFrameBound_ = this.renderFrame_.bind(this);
+  this.renderFrameBound_ = this.renderFrame_.bind(this);
   this.frequency_ = 8;
   this.effects_ = new Effects();
   this.tracker_ = new FaceTracker(ccv);
@@ -116,7 +116,60 @@ HangoutsPage.prototype = {
 
 HangoutsPage.prototype.enter = function() {
   AppPage.prototype.enter.call(this);
-  chrome.runtime.sendMessage({name: 'enterHangoutsPage'});
+  navigator.webkitGetUserMedia(
+    {video: true},
+    function(localMediaStream) {
+      if (this.isVideoUsing)
+        return;
+      // Register the events.
+      var doc = this.root.document;
+      this.handlers.add(doc.querySelector('.prev-effect'), 'click', function() {
+        this.effectIndex_ =
+            (this.effectIndex_ + this.effects_.data.length - 1) %
+            this.effects_.data.length;
+      }.bind(this));
+      this.handlers.add(doc.querySelector('.next-effect'), 'click', function() {
+        this.effectIndex_ =
+            (this.effectIndex_ + this.effects_.data.length + 1) %
+            this.effects_.data.length;
+      }.bind(this));
+      // Initialize the video.
+      var URL = this.root.mainWindow_.contentWindow.URL;
+      var url = URL.createObjectURL(localMediaStream);
+      var videoSource = this.element_.querySelector('.video-source');
+      videoSource.src = url;
+      videoSource.play();
+      this.videoSource_ = videoSource;
+      this.mediaStream_ = localMediaStream;
+      this.canvas_ = this.element_.querySelector('.video-canvas');
+      this.canvasContext_ = this.canvas_.getContext('2d');
+      this.effectIndex_ = 0;
+      this.frame_ = 0;
+      this.track_ = {faces:[]};
+      this.effects_.init();
+      this.renderFrame_();
+    }.bind(this),
+    function(err) {
+      console.error('Failed to initialize the camera.');
+      // Nothing to do when it failed.
+    }
+  );
+};
+
+HangoutsPage.prototype.renderFrame_ = function() {
+  if (!this.isVideoUsing)
+    return;
+  this.canvasContext_.drawImage(
+      this.videoSource_, 0, 0, this.canvas_.width, this.canvas_.height);
+  var effect = this.effects_.data[this.effectIndex_];
+  if (effect.tracks && this.frame_ % this.frequency_ == 0) {
+    this.track_ = this.tracker_.track(this.canvas_);
+  }
+  this.effects_.advance(this.canvas_);
+  this.effects_.data[this.effectIndex_].filter(
+      this.canvas_, this.canvas_, this.frame_++, this.track_);
+  this.root.mainWindow_.contentWindow.requestAnimationFrame(
+      this.renderFrameBound_);
 };
 
 HangoutsPage.prototype.leave = function() {
