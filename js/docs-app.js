84da56aad91db9ae33b7f0fc7adcde0f4ad238e7
Daichi Hirono <hirono@chromium.org>
2013-07-29 15:21:17 -0700

    Fixed the design of Docs.app. close #47.

--- js/docs-app.js
+++ js/docs-app.js
@@ -8,6 +8,13 @@ DocsApp.prototype = {
 
 DocsApp.prototype.initDocument = function() {
   App.prototype.initDocument.call(this);
+
+  // Set padding as the smae size of scroll width.
+  var paperContainer = this.get('.paper-container');
+  var scrollSize =
+      this.get('#docs-main').clientWidth -
+      paperContainer.clientWidth;
+  paperContainer.style.paddingLeft = scrollSize + 'px';
 };
 
 new DocsApp().start();
