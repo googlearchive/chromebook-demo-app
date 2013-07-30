9c509419e696a03c96b93e79c1dda6160a21ac85
Daichi Hirono <hirono@chromium.org>
2013-07-30 10:08:21 -0700

    Added two extension IDs to the Store.app. #36.

--- js/store-app.js
+++ js/store-app.js
@@ -8,6 +8,16 @@ StoreApp.prototype = {
 
 StoreApp.prototype.initDocument = function() {
   App.prototype.initDocument.call(this);
+
+  // App tiles.
+  var apps = this.document.querySelectorAll('.app');
+  for (var i = 0; i < apps.length; i++) {
+    apps[i].addEventListener('click', function(app) {
+      var id = app.getAttribute('data-id');
+      console.log(id);
+      chrome.runtime.sendMessage(HELPER_EXTENSION_ID, {name: 'launch', id: id});
+    }.bind(this, apps[i]));
+  }
 };
 
 new StoreApp().start();
